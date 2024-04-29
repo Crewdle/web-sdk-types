e6ac447e77b8e33913e85c86d39a089367bcc44d
Vincent Lamanna <vincent@crewdle.com>
2024-04-29 11:39:05 -0400

    Add agent version

--- dist/core/node/agent/AgentCapacity.d.ts
+++ dist/core/node/agent/AgentCapacity.d.ts
@@ -1,19 +1,65 @@
+/**
+ * The agent capacity interface used for reporting the agent's capacity.
+ */
 export interface IAgentCapacity {
+    /**
+     * The agent version.
+     */
+    version: string;
+    /**
+     * The agent's MAC address.
+     */
     macAddress: string;
+    /**
+     * The agent's CPU capacity.
+     */
     cpu: {
+        /**
+         * The number of CPU cores.
+         */
         cores: number;
+        /**
+         * The current CPU load.
+         */
         load: number;
     };
+    /**
+     * The agent's GPU capacity.
+     */
     gpu: {
+        /**
+         * The number of GPU cores.
+         */
         cores: number;
+        /**
+         * The current GPU load.
+         */
         load: number;
     };
+    /**
+     * The agent's memory capacity.
+     */
     memory: {
+        /**
+         * The total memory available.
+         */
         total: number;
+        /**
+         * The available memory.
+         */
         available: number;
     };
+    /**
+     * The agent's storage capacity.
+     */
     storage: {
+        /**
+         * The total storage available.
+         */
         total: number;
+        /**
+         * The available storage.
+         */
         available: number;
     };
 }
