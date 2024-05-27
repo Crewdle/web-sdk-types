f215d7f59695a4309b39dc5e1ad696d7c47a4ab5
Vincent Lamanna <vincent@crewdle.com>
2024-05-27 13:13:46 -0400

    CA-2232 Add variable to machine learning constructor

--- dist/machine-learning/MachineLearningConnectorConstructor.d.ts
+++ dist/machine-learning/MachineLearningConnectorConstructor.d.ts
@@ -1,6 +1,7 @@
+import { VectorDatabaseConnectorConstructor } from 'vector-database';
 import { IMachineLearningConnector } from '.';
 /**
  * The machine learning connector constructor type.
  * @category Machine Learning
  */
-export type MachineLearningConnectorConstructor = new () => IMachineLearningConnector;
+export type MachineLearningConnectorConstructor = new (vectorDatabaseConnector: VectorDatabaseConnectorConstructor) => IMachineLearningConnector;
--- src/machine-learning/MachineLearningConnectorConstructor.ts
+++ src/machine-learning/MachineLearningConnectorConstructor.ts
@@ -1,7 +1,8 @@
+import { VectorDatabaseConnectorConstructor } from 'vector-database';
 import { IMachineLearningConnector } from '.';
 
 /**
  * The machine learning connector constructor type.
  * @category Machine Learning
  */
-export type MachineLearningConnectorConstructor = new () => IMachineLearningConnector;
+export type MachineLearningConnectorConstructor = new (vectorDatabaseConnector: VectorDatabaseConnectorConstructor) => IMachineLearningConnector;
