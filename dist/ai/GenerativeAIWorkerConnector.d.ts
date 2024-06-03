519fda5481b5bf322da9cc108f5b9fa475e784b3
Vincent Lamanna <vincent@crewdle.com>
2024-06-03 14:35:21 -0400

    Make models optional in initialize

--- dist/ai/GenerativeAIWorkerConnector.d.ts
+++ dist/ai/GenerativeAIWorkerConnector.d.ts
@@ -9,7 +9,7 @@ export interface IGenerativeAIWorkerConnector extends IJobWorkerConnector {
      * @param llmModel The path to the LLM model.
      * @param similarityModel The path to the similarity model.
      */
-    initialize(llmModel: string, similarityModel: string): Promise<void>;
+    initialize(llmModel?: string, similarityModel?: string): Promise<void>;
     /**
      * Add content to the machine learning model.
      * @param content The content to add.
