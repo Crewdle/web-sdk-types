75befb8178fff7aeebf6f133092f7ed2dddffea1
Vincent Lamanna <vincent@crewdle.com>
2024-10-03 20:57:40 -0400

    Add parameters to external storage constructor

--- src/external-storage/ExternalStorageConnectionConnectorConstructor.ts
+++ src/external-storage/ExternalStorageConnectionConnectorConstructor.ts
@@ -4,4 +4,4 @@ import { IExternalStorageConnectionConnector } from '.';
  * The external storage connection connector constructor type.
  * @category External Storage
  */
-export type ExternalStorageConnectionConnectorConstructor = new () => IExternalStorageConnectionConnector;
+export type ExternalStorageConnectionConnectorConstructor = new (siteId: string, driveId: string, accessToken: string, refreshToken: () => Promise<string>) => IExternalStorageConnectionConnector;
